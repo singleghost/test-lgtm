70fefdfd59f909536043713885b0f68e47e3e54a
栈长 <jundong.xjd@antfin.com>
2019-09-02 16:05:34 +0800

    测试+=运算符

--- src/main.c
+++ src/main.c
@@ -2,12 +2,5 @@
 int main() {
 	unsigned op1 = 0xffffffff;
 	unsigned op2 = 2;
-	unsigned result;
-	result = op1 + op2;
-	if(result < op2) {
-		printf("overflowed!\n");
-	}
-	else {
-		printf("not overflow!\n");
-	}
+	op1 += op2;
 }
