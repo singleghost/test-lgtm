5770aa0533fc89c8879124a3482f41fbfc22e954
栈长 <jundong.xjd@antfin.com>
2019-09-24 14:14:14 +0800

    测试两个 integer 相加的结果被用于之后的比较运算符

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main() {
-	unsigned op1 = 0xffffffff;
-	unsigned op2 = 2;
-	op1 += op2;
-	return op1;
+	unsigned op1 = random();
+	unsigned op2 = random();
+	unsigned op3 = random();
+	op1 = op2 + op3;
+	if(op1 < 100) { //bad overflow check
+		printf("not overflowed!\n");
+	}
+	else {
+		printf("overflowed\n");
+	}
+	return 0;
 }
